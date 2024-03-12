eddc50c47266fb640959aa8a6cead4057f7f14cd
M0H1K4N <nejer911@gmail.com>
2024-03-12 19:31:22 +0400

    add

--- test.cpp
+++ test.cpp
@@ -1,15 +1,18 @@
 #include <iostream>
 using namespace std;
 
-typedef std::string text_t;
+// typedef std::string text_t;
+
+using text_t = std::string;
+using number_t = int;
 
 int main()
 {   
 
     text_t firstName  = "Luka";
-
-
-    cout << "Hello world" << "My name is " << firstName <<endl;
+    number_t age = 21;
+    cout << "Hello world " << "My name is " << firstName <<endl;
+    cout<< "And I am " << age << " years old" << endl;
 
     return 0;
 }
